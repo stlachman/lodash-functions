f8d19e20280e32bd820a9cae503714e5f0d102d8
Seth Lachman <lachmanseth17@gmail.com>
2020-03-04 09:41:11 -0500

    Added test for difference.

--- src/difference/difference.test.js
+++ src/difference/difference.test.js
@@ -7,3 +7,10 @@ it("returns new array of filtered values", () => {
 it("returns an empty array if all values match", () => {
   expect(difference([2, 1], [2, 1])).toEqual([]);
 });
+
+it("returns the array unchanged if no values match", () => {
+  expect(difference(["hi", "hello"], ["no", "difference"])).toEqual([
+    "hi",
+    "hello"
+  ]);
+});
